6404efe1271b8e374e81d60ca9fe92fff77b9f33
Camille Fourcade <ystos.gaming@gmail.com>
2022-03-31 15:27:33 +0200

    test ggl

--- app/controllers/blagues_controller.rb
+++ app/controllers/blagues_controller.rb
@@ -25,7 +25,7 @@ class BlaguesController < ApplicationController
 
     respond_to do |format|
       if @blague.save
-        format.html { redirect_to blague_url(@blague), notice: "Blague was successfully created." }
+        format.html { redirect_to " ../acceuil/index", notice: "Blague was successfully created." }
         format.json { render :show, status: :created, location: @blague }
       else
         format.html { render :new, status: :unprocessable_entity }
